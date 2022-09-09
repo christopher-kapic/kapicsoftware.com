11fea74ef9cc18578e5c0082a85d910daea6ca6d
Christopher Kapic <christopherkapic@gmail.com>
2022-09-09 10:07:26 -0500

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,40 @@
+export const config = {
+  site: "https://kapicsoftware.com",
+  contact: {
+    email: "christopher@kapicmedia.com",
+    socials: {
+      facebook: undefined,
+      twitter: "https://twitter.com/kapicode",
+      instagram: undefined,
+      github: "https://github.com/christopher-kapic",
+      linkedin: "https://linkedin.com/in/christopher-kapic",
+    },
+    phone: "(555) 555-5555", // string
+    address: {
+      street: "1234 Fake St",
+      city: "Green Bay",
+      state: "WI", // abbreviation
+      zip: "54173",
+    },
+  },
+  navbarLinks: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+  ],
+  footerLinks: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+    },
+  ]
+};
