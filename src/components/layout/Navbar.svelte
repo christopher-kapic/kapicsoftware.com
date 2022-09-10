beb1c3ea7ce35a948d17fa8257e3f4eb0e3957e2
Christopher Kapic <christopherkapic@gmail.com>
2022-09-10 14:30:40 -0400

    image width and height for logos

--- src/components/layout/Navbar.svelte
+++ src/components/layout/Navbar.svelte
@@ -15,7 +15,7 @@
 <section class="bg-white">
   <nav class="flex justify-between p-6 px-4 desktop-navbar">
     <a href={logoHref} class="flex items-center" rel="prefetch">
-      <img class="h-8 mr-2" width={32} height={32} src={logo} alt={`${title} logo`} />
+      <img class="h-8 mr-2" width="32" height="32" src={logo} alt={`${title} logo`} />
       <span
         class="text-gray-200 font-extrabold text-2xl hover:text-gray-300 transition hover:underline decoration-primary"
         >{title}</span
@@ -82,7 +82,7 @@
         <div class="flex flex-col justify-between h-full">
           <div>
             <a class="inline-block mb-6 flex items-center" href="/">
-              <img class="h-8 mr-2" src={logo} alt={`${title} logo`} />
+              <img class="h-8 mr-2" width="32" height="32" src={logo} alt={`${title} logo`} />
               <span
                 class="text-neutral font-extrabold text-2xl hover:text-neutral-focus transition hover:underline decoration-primary"
                 >{title}</span
